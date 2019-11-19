471200592f250c2de77cd4eb1a965da929f210fb
PatrikBuhr <patrik.buhr@est.tech>
2019-11-19 09:56:44 +0100

    Dark mode default
    Decreased size of copyright text.
    Bugfix, removed minsize of instance table,
    Bugfix, disabling of sorting of types. This is due to a bug in angular.
    
    Change-Id: I2fdef23d700043268983decfc64830c19ad97f0d
    Issue-ID: NONRTRIC-61
    Signed-off-by: PatrikBuhr <patrik.buhr@est.tech>

--- dashboard/webapp-frontend/src/app/services/ui/ui.service.ts
+++ dashboard/webapp-frontend/src/app/services/ui/ui.service.ts
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,8 +17,8 @@
  * limitations under the License.
  * ========================LICENSE_END===================================
  */
-import {Injectable} from '@angular/core';
-import {BehaviorSubject} from 'rxjs';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class UiService {
@@ -27,6 +27,6 @@ export class UiService {
 
   constructor() {
     // TODO: if the user is signed in get the default value from Firebase
-    this.darkModeState = new BehaviorSubject<boolean>(false);
+    this.darkModeState = new BehaviorSubject<boolean>(true);
   }
 }
