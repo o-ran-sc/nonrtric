706b4ef9875f13699989809f6ffdfafe4e0ca419
PatrikBuhr <patrik.buhr@est.tech>
2019-11-28 10:58:54 +0100

    Policy-agent skeleton
    Change-Id: I23ffaf459b2f5f2685ad81ad6726556491b8b3b7
    Issue-ID: NONRTRIC-76
    Signed-off-by: PatrikBuhr <patrik.buhr@est.tech>

---
+++ policy-agent/src/main/java/org/oransc/policyagent/configuration/ApplicationConfig.java
@@ -0,0 +1,111 @@
+/*-
+ * ========================LICENSE_START=================================
+ * O-RAN-SC
+ * %%
+ * Copyright (C) 2019 Nordix Foundation
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================LICENSE_END===================================
+ */
+
+package org.oransc.policyagent.configuration;
+
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.TypeAdapterFactory;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Properties;
+import java.util.ServiceLoader;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.oransc.policyagent.exceptions.ServiceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@EnableConfigurationProperties
+@ConfigurationProperties("app")
+public class ApplicationConfig {
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationConfig.class);
+
+    @Value("#{systemEnvironment}")
+    Properties systemEnvironment;
+
+    @NotEmpty
+    private String filepath;
+
+    @Autowired
+    public ApplicationConfig() {
+    }
+
+    public synchronized void setFilepath(String filepath) {
+        this.filepath = filepath;
+    }
+
+    /**
+     * Reads the cloud configuration.
+     */
+    public void initialize() {
+        loadConfigurationFromFile();
+    }
+
+    Mono<Environment.Variables> getEnvironment(Properties systemEnvironment) {
+        return Environment.readEnvironmentVariables(systemEnvironment);
+    }
+
+    /**
+     * Reads the configuration from file.
+     */
+    void loadConfigurationFromFile() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        ServiceLoader.load(TypeAdapterFactory.class).forEach(gsonBuilder::registerTypeAdapterFactory);
+
+        try (InputStream inputStream = createInputStream(filepath)) {
+            JsonParser parser = new JsonParser();
+            JsonObject rootObject = getJsonElement(parser, inputStream).getAsJsonObject();
+            if (rootObject == null) {
+                throw new JsonSyntaxException("Root is not a json object");
+            }
+            ApplicationConfigParser appParser = new ApplicationConfigParser();
+            appParser.parse(rootObject);
+            logger.info("Local configuration file loaded: {}", filepath);
+        } catch (JsonSyntaxException | ServiceException | IOException e) {
+            logger.trace("Local configuration file not loaded: {}", filepath, e);
+        }
+    }
+
+    JsonElement getJsonElement(JsonParser parser, InputStream inputStream) {
+        return parser.parse(new InputStreamReader(inputStream));
+    }
+
+    InputStream createInputStream(@NotNull String filepath) throws IOException {
+        return new BufferedInputStream(new FileInputStream(filepath));
+    }
+
+}
---
+++ policy-agent/src/test/java/org/oransc/policyagent/ApplicationTest.java
@@ -0,0 +1,49 @@
+/*-
+ * ========================LICENSE_START=================================
+ * O-RAN-SC
+ * %%
+ * Copyright (C) 2019 Nordix Foundation
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================LICENSE_END===================================
+ */
+package org.oransc.policyagent;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ApplicationTest {
+
+    @LocalServerPort
+    private int port;
+
+    private RestTemplate restTemplate = new RestTemplate();
+
+    @Test
+    public void getPolicy() throws Exception {
+        String cmd = "/policy?type=type3&instance=xxx";
+        String rsp = this.restTemplate.getForObject("http://localhost:" + port + cmd, String.class);
+        System.out.println("*** rsp " + rsp);
+        assertThat(rsp).contains("type3");
+    }
+
+}
