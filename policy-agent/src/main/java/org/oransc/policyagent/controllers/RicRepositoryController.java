2aa1689637366d03a1252afadf93be937c1ec644
PatrikBuhr <patrik.buhr@est.tech>
2019-12-06 10:36:35 +0100

    Continue work with PolicyControl
    Added a Bean factory
    
    Change-Id: I6351ab07614a3345393d2415cd3adb4b14a96eca
    Issue-ID: NONRTRIC-76
    Signed-off-by: PatrikBuhr <patrik.buhr@est.tech>

---
+++ policy-agent/src/main/java/org/oransc/policyagent/controllers/RicRepositoryController.java
@@ -0,0 +1,98 @@
+/*-
+ * ========================LICENSE_START=================================
+ * O-RAN-SC
+ * %%
+ * Copyright (C) 2019 Nordix Foundation
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================LICENSE_END===================================
+ */
+package org.oransc.policyagent.controllers;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+import java.util.Optional;
+import java.util.Vector;
+
+import org.oransc.policyagent.configuration.ApplicationConfig;
+import org.oransc.policyagent.configuration.RicConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RicRepositoryController {
+
+    private final ApplicationConfig appConfig;
+    private static Gson gson = new GsonBuilder() //
+        .serializeNulls() //
+        .create(); //
+
+    @Autowired
+    RicRepositoryController(ApplicationConfig config) {
+        this.appConfig = config;
+    }
+
+    /**
+     * Example: http://localhost:8080/rics?managedElementId=kista_1
+     */
+    @GetMapping("/ric")
+    @ApiOperation(value = "Returns the name of a RIC managing one Mananged Element")
+    @ApiResponses(
+        value = { //
+            @ApiResponse(code = 200, message = "RIC is fond"), //
+            @ApiResponse(code = 404, message = "RIC is not fond") //
+        })
+    public ResponseEntity<String> getRic(
+        @RequestParam(name = "managedElementId", required = false, defaultValue = "") String managedElementId) {
+
+        Optional<RicConfig> config = appConfig.lookupRicConfigForManagedElement(managedElementId);
+
+        if (config.isPresent()) {
+            return new ResponseEntity<>(config.get().name(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /**
+     * @return a Json array of all RIC data
+     *         Example: http://localhost:8080/ric
+     */
+    @GetMapping("/rics")
+    @ApiOperation(value = "Returns defined NearRT RIC:s as Json")
+    @ApiResponses(
+        value = { //
+            @ApiResponse(code = 200, message = "OK") //
+        })
+    public ResponseEntity<String> getRics() {
+        Vector<RicInfo> result = new Vector<RicInfo>();
+        for (RicConfig ricConfig : this.appConfig.getRicConfigs()) {
+            RicInfo ric = ImmutableRicInfo.builder() //
+                .managedElementIds(ricConfig.managedElementIds()) //
+                .name(ricConfig.name()) //
+                .build();
+            result.add(ric);
+        }
+        return new ResponseEntity<>(gson.toJson(result), HttpStatus.OK);
+    }
+
+}
