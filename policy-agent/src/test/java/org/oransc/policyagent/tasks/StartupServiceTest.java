7f4d259582ad5648bb9fff3233710c855d0e3d2d
elinuxhenrik <henrik.b.andersson@est.tech>
2019-12-11 10:30:25 +0100

    Refine test of StartupService
    Change-Id: I94e29eea29f8cefe2c1121e8ff99052c571a2e01
    Issue-ID: NONRTRIC-81
    Signed-off-by: elinuxhenrik <henrik.b.andersson@est.tech>

--- policy-agent/src/test/java/org/oransc/policyagent/tasks/StartupServiceTest.java
+++ policy-agent/src/test/java/org/oransc/policyagent/tasks/StartupServiceTest.java
@@ -21,11 +21,11 @@
 package org.oransc.policyagent.tasks;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
+import static org.oransc.policyagent.repository.Ric.RicState.ACTIVE;
 
 import java.util.Vector;
 
@@ -38,41 +38,74 @@ import org.oransc.policyagent.exceptions.ServiceException;
 import org.oransc.policyagent.repository.ImmutablePolicyType;
 import org.oransc.policyagent.repository.PolicyType;
 import org.oransc.policyagent.repository.PolicyTypes;
+import org.oransc.policyagent.repository.Ric;
 import org.oransc.policyagent.repository.Rics;
 
 public class StartupServiceTest {
+    private static final String FIRST_RIC_NAME = "first";
+    private static final String FIRST_RIC_URL = "firstUrl";
+    private static final String SECOND_RIC_NAME = "second";
+    private static final String SECOND_RIC_URL = "secondUrl";
+
+    private static final String PLOCY_TYPE_1_NAME = "type1";
+    private static final String POLICY_TYPE_2_NAME = "type2";
+
     ApplicationConfig appConfigMock;
 
     @Test
     public void startup_allOk() throws ServiceException {
         ApplicationConfig appConfigMock = mock(ApplicationConfig.class);
         Vector<RicConfig> ricConfigs = new Vector<>(2);
-        Vector<String> firstNodes = new Vector<String>(1);
-        firstNodes.add("nodeA");
-        ricConfigs.add(ImmutableRicConfig.builder().name("first").managedElementIds(firstNodes).baseUrl("url").build());
+        ricConfigs.add(getRicConfig(FIRST_RIC_NAME, FIRST_RIC_URL, "nodeA"));
+        ricConfigs.add(getRicConfig(SECOND_RIC_NAME, SECOND_RIC_URL, "nodeB", "nodeC"));
         when(appConfigMock.getRicConfigs()).thenReturn(ricConfigs);
+
+        Vector<PolicyType> firstTypes = new Vector<>();
+        PolicyType type1 = ImmutablePolicyType.builder().name(PLOCY_TYPE_1_NAME).jsonSchema("{}").build();
+        firstTypes.add(type1);
+        Vector<PolicyType> secondTypes = new Vector<>();
+        secondTypes.add(type1);
+        PolicyType type2 = ImmutablePolicyType.builder().name(POLICY_TYPE_2_NAME).jsonSchema("{}").build();
+        secondTypes.add(type2);
+        RicClient ricClientMock = mock(RicClient.class);
+        when(ricClientMock.getPolicyTypes(FIRST_RIC_URL)).thenReturn(firstTypes);
+        when(ricClientMock.getPolicyTypes("secondUrl")).thenReturn(secondTypes);
 
         Rics rics = new Rics();
         PolicyTypes policyTypes = new PolicyTypes();
-
-        Vector<PolicyType> types = new Vector<>();
-        PolicyType type = ImmutablePolicyType.builder().name("type1").jsonSchema("{}").build();
-        types.add(type);
-        RicClient ricClientMock = mock(RicClient.class);
-        when(ricClientMock.getPolicyTypes(anyString())).thenReturn(types);
-
         StartupService serviceUnderTest = new StartupService(appConfigMock, rics, policyTypes, ricClientMock);
 
         serviceUnderTest.startup();
 
-        verify(ricClientMock).deleteAllPolicies("url");
-        verify(ricClientMock).getPolicyTypes("url");
+        verify(ricClientMock).deleteAllPolicies(FIRST_RIC_URL);
+        verify(ricClientMock).getPolicyTypes(FIRST_RIC_URL);
+        verify(ricClientMock).deleteAllPolicies(SECOND_RIC_URL);
+        verify(ricClientMock).getPolicyTypes(SECOND_RIC_URL);
         verifyNoMoreInteractions(ricClientMock);
 
-        assertEquals("Correct nymber of Rics not added to Rics", 1, rics.size());
-        assertEquals("Not correct Ric added to Rics", "first", rics.getRic("first").name());
+        assertEquals("Correct nymber of Rics not added to Rics", 2, rics.size());
+        Ric firstRic = rics.getRic(FIRST_RIC_NAME);
+        assertEquals("Not correct Ric \"" + FIRST_RIC_NAME + "\" added to Rics", FIRST_RIC_NAME, firstRic.name());
+        assertEquals("Not correct state for \"" + FIRST_RIC_NAME + "\"", ACTIVE, firstRic.state());
+        Ric secondRic = rics.getRic(SECOND_RIC_NAME);
+        assertEquals("Not correct Ric \"" + SECOND_RIC_NAME + "\" added to Rics", SECOND_RIC_NAME, secondRic.name());
+        assertEquals("Not correct state for \"" + SECOND_RIC_NAME + "\"", ACTIVE, secondRic.state());
 
-        assertEquals("Not correct number of policy types added.", 1, policyTypes.size());
-        assertEquals("Not correct type added.", type, policyTypes.getType(type.name()));
+        assertEquals("Not correct number of policy types added.", 2, policyTypes.size());
+        assertEquals("Not correct type added.", type1, policyTypes.getType(PLOCY_TYPE_1_NAME));
+        assertEquals("Not correct type added.", type2, policyTypes.getType(POLICY_TYPE_2_NAME));
+    }
+
+    private RicConfig getRicConfig(String name, String baseUrl, String... nodeNames) {
+        Vector<String> managedNodes = new Vector<String>(1);
+        for (String nodeName : managedNodes) {
+            managedNodes.add(nodeName);
+        }
+        ImmutableRicConfig ricConfig = ImmutableRicConfig.builder() //
+            .name(name) //
+            .managedElementIds(managedNodes) //
+            .baseUrl(baseUrl) //
+            .build();
+        return ricConfig;
     }
 }
