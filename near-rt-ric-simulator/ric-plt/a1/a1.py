9377e7b8b862711664337b193a748ed71fe75255
maximesson <maxime.bonneau@est.tech>
2019-12-11 16:53:33 +0100

    New version of NearRT-RIC simulator
    Change-Id: I9b14b8e6a32974e92a57af994762be064b04b7a7
    Issue-ID: NONRTRIC-78
    Signed-off-by: maximesson <maxime.bonneau@est.tech>

---
+++ near-rt-ric-simulator/ric-plt/a1/a1.py
@@ -0,0 +1,219 @@
+#!/u:sr/bin/env python3
+import connexion
+import copy
+import datetime
+import json
+import logging
+import requests
+
+from connexion import NoContent
+from flask import Flask, escape, request
+from jsonschema import validate
+from random import random, choice
+from var_declaration import policy_instances, policy_types, policy_status, notification_destination, notificationDestination
+
+error = {}
+params = {}
+
+def get_all_policies():
+  all_p = copy.deepcopy(policy_instances)
+  all_policies = []
+  for i in all_p.keys():
+    all_p[i]["enforceStatus"] = policy_status[i]["enforceStatus"]
+    all_policies.insert(len(all_policies)-1, all_p[i])
+  return(all_policies, 200)
+
+def put_policy(policyId):
+  data = request.data.decode("utf-8")
+  data = data.replace("'", "\"")
+  data = json.loads(data)
+  ps = {}
+  if data["policyTypeId"] not in list(policy_types.keys()):
+    reset_error()
+    error["title"] = "The policy type provided does not exist."
+    error["status"] = 404
+    error["detail"] = "The policy type " + data["policyTypeId"] + " is not defined as a policy type."
+    params["param"] = "policyTypeId"
+    error["invalidParams"] = params
+    return(error, 404)
+  if data["policyId"] in list(policy_instances.keys()):
+    if data["policyClause"]["scope"] != policy_instances[data["policyId"]]["policyClause"]["scope"]:
+      reset_error()
+      error["title"] = "The policy already exists with a different scope."
+      error["status"] = 404
+      error["detail"] = "The policy put involves a modification of the existing scope, which is not allowed."
+      params["param"] = "scope"
+      error["invalidParams"] = params
+      return(error, 404)
+  if data["policyId"] != policyId:
+    reset_error()
+    error["title"] = "Wrong policy identity."
+    error["status"] = 400
+    error["detail"] = "The policy instance's identity does not match with the one specified in the address."
+    params["param"] = "policyId"
+    params["reason"] = "The policy identity " + data["policyId"] + " is different from the address: " + policyId
+    error["invalidParams"] = params
+    return(error, 400)
+  for i in list(policy_instances.keys()):
+    if data["policyClause"] == policy_instances[i]["policyClause"] and \
+       data["policyTypeId"] == policy_instances[i]["policyTypeId"] and \
+       data["notificationDestination"] == policy_instances[i]["notificationDestination"]:
+      reset_error()
+      error["title"] = "The policy already exists with a different id."
+      error["status"] = 404
+      error["detail"] = "No action has been taken. The id of the existing policy instance is: " + i + "."
+      return(error, 404)
+  pt = data["policyTypeId"]
+  schema = policy_types[pt]
+  try:
+    print("I promise I'm trying!")
+    validate(instance=data, schema=schema)
+  except ValueError:
+    print("But I failed...")
+    reset_error()
+    error["title"] = "The json does not validate against the schema."
+    error["status"] = 400
+    return(error, 400)
+  if policyId in policy_instances.keys():
+    code = 201
+  else:
+    code = 200
+  policy_instances[policyId] = data
+  policy_status[policyId] = set_status("UNDEFINED")
+  notification_destination[policyId] = data["notificationDestination"]
+  return(policy_instances[policyId], code)
+
+def set_status(*args):
+  ps = {}
+  if len(args) == 0:
+    rand_status = randomise_status()
+    ps["policyId"] = policyId
+    ps["enforceStatus"] = rand_status
+    if rand_status == "NOT_ENFORCED":
+      rand_reason = randomise_reason()
+      ps["enforceReason"] = rand_reason
+  if args[0] in ["UNDEFINED", "ENFORCED", "NOT_ENFORCED"]:
+    ps["enforceStatus"] = args[0]
+  else:
+    reset_error()
+    error["title"] = "Wrong enforceStatus."
+    error["status"] = 400
+    params["param"] = "enforceStatus"
+    params["reason"] = "enforceStatus should be one of \"UNDEFINED\", \"ENFORCED\" or \"NOT_ENFORCED\""
+    error["invalidParams"] = params
+    return(error, 400)
+  if args[0] == "NOT_ENFORCED":
+    if args[1] in ["100", "200", "300", "800"]:
+      ps["enforceReason"] = args[1]
+    else:
+      reset_error()
+      error["title"] = "Wrong enforceReason."
+      error["status"] = 400
+      params["param"] = "enforceReason"
+      params["reason"] = "enforceReason should be one of \"100\", \"200\", \"300\" or \"800\""
+      error["invalidParams"] = params
+      return(error, 400)
+  return ps
+
+def get_policy(policyId):
+  if policyId in policy_instances.keys():
+    res = policy_instances[policyId]
+    res["enforceStatus"] = policy_status[policyId]["enforceStatus"]
+    return(res, 200)
+  else:
+    reset_error()
+    error["title"] = "The requested policy does not exist."
+    error["status"] = 404
+    params["param"] = "policyId"
+    error["invalidParams"] = params
+    return(error, 404)
+
+def delete_policy(policyId):
+  if policyId in policy_instances.keys():
+    policy_instances.pop(policyId)
+    policy_status.pop(policyId)
+    return(None, 204)
+  else:
+    reset_error()
+    error["title"] = "The policy identity does not exist."
+    error["status"] = 404
+    error["detail"] = "No policy instance has been deleted."
+    params["param"] = "policyId"
+    error["invalidParams"] = params
+    return(error, 404)
+
+def get_all_policy_identities():
+  return(list(policy_instances.keys()), 200)
+
+def randomise_status():
+  x = random()
+  if x > 0.5001:
+    res = "ENFORCED"
+  elif x < 0.4999:
+    res = "NOT_ENFORCED"
+  else:
+    res = "UNDEFINED"
+  return res
+
+def randomise_reason():
+  options = ["100", "200", "300", "800"]
+  return choice(options)
+
+def get_all_policy_status():
+  all_s = copy.deepcopy(policy_status)
+  all_status = []
+  for i in all_s.keys():
+    all_s[i]["policyId"] = i
+    all_status.insert(len(all_status)-1, all_s[i])
+  return(all_status, 200)
+
+def get_policy_status(policyId):
+  return(policy_status[policyId], 200)
+
+def get_all_policytypes():
+  all_policytypes = []
+  for i in policy_types.keys():
+    all_policytypes.insert(len(all_policytypes)-1, policy_types[i])
+  return(all_policytypes, 200)
+
+def get_all_policytypes_identities():
+  return(list(policy_types.keys()), 200)
+
+def get_policytypes(policyTypeId):
+  if policyTypeId in policy_types.keys():
+    return(policy_types[policyTypeId], 200)
+  else:
+    reset_error()
+    error["title"] = "The requested policy type does not exist."
+    error["status"] = 404
+    params["param"] = "policyTypeId"
+    error["invalidParams"] = params
+    return(error, 404)
+
+def put_policytypes_subscription():
+  global notificationDestination
+  data = request.data.decode("utf-8")
+  data = data.replace("'", "\"")
+  data = json.loads(data)
+  if not notificationDestination:
+    notificationDestination["notificationDestionation"] = data
+    return(None, 201)
+  else:
+    notificationDestination["notificationDestionation"] = data
+    return(None, 200)
+
+def get_policytypes_subscription():
+  if not notificationDestination:
+    reset_error()
+    error["title"] = "The notification destination has not been defined."
+    error["status"] = 404
+    params["param"] = "notificationDestination"
+    error["invalidParams"] = params
+    return(error, 404)
+  else:
+    return(notificationDestination["notificationDestionation"], 200)
+
+def reset_error():
+  error.clear()
+  params.clear()
+
---
+++ near-rt-ric-simulator/ric-plt/a1/main.py
@@ -0,0 +1,86 @@
+import connexion
+import fileinput
+import json
+import sys
+
+from flask import Flask, escape, request, make_response
+from var_declaration import policy_instances, policy_types, policy_status
+
+app = connexion.App(__name__, specification_dir='.')
+
+@app.route('/policytypes/<string:policyTypeId>', methods=['PUT','DELETE'])
+def policy_type(policyTypeId):
+  if request.method == 'PUT':
+    data = request.data.decode("utf-8")
+    data = data.replace("'", "\"")
+    data = json.loads(data)
+    policy_types[policyTypeId] = data
+    return ('The policy type was either created or updated for policy type id: ' + policyTypeId)
+  elif request.method == 'DELETE':
+    if policyTypeId in policy_types.keys():
+      policy_types.pop(policyTypeId)
+      return make_response("policy type successfully deleted for policy type id: " + policyTypeId, 200)
+    else:
+      return make_response("No policy type defined for the specified id", 404)
+
+@app.route('/', methods=['GET'])
+def test():
+  return(str(list(policy_types.keys())), 200)
+
+@app.route('/deleteinstances', methods=['DELETE'])
+def delete_instances():
+  global policy_instances
+  global policy_status
+  policy_instances.clear()
+  policy_status.clear()
+  return("All policy instances deleted", 200)
+
+@app.route('/deletetypes', methods=['DELETE'])
+def delete_types():
+  global policy_types
+  policy_types.clear()
+  return("All policy types deleted", 200)
+
+@app.route('/<string:policyId>/<string:enforceStatus>', methods=['PUT'])
+def set_status(policyId, enforceStatus):
+  if policyId in policy_instances.keys():
+    if enforceStatus in ["UNDEFINED", "ENFORCED", "NOT_ENFORCED"]:
+      policy_status.pop(policyId) 
+      ps = {}
+      ps["policyId"] = policyId
+      ps["enforceStatus"] = enforceStatus
+      policy_status[policyId] = ps
+      return("Status updated for policy: " + policyId, 200)
+    else:
+      return("enforceStatus should be one of \"UNDEFINED\", \"ENFORCED\" or \"NOT_ENFORCED\"", 400)
+  else:
+    return("The policy id does not correspond to any existing policy instance", 400)
+
+@app.route('/<string:policyId>/<string:enforceStatus>/<string:enforceReason>', methods=['PUT'])
+def set_status_with_reason(policyId, enforceStatus, enforceReason):
+  if policyId in policy_instances.keys():
+    if enforceStatus == "NOT_ENFORCED":
+      if enforceReason in ["100", "200", "300", "800"]:
+        policy_status.pop(policyId) 
+        ps = {}
+        ps["policyId"] = policyId
+        ps["enforceStatus"] = enforceStatus
+        ps["enforceReason"] = enforceReason
+        policy_status[policyId] = ps
+        return("Status updated for policy: " + policyId, 200)
+      else: 
+        return("enforceReason should be one of \"100\", \"200\", \"300\" or \"800\"", 400)
+    else:
+      return("A status provided together with an enforcement reason should be \"NOT_ENFORCED\"", 400)
+  else:
+    return("The policy id does not correspond to any existing policy instance", 404)
+
+
+port_number = 8085
+if len(sys.argv) >= 2:
+  if isinstance(sys.argv[1], int):
+    port_number = sys.argv[1]
+
+app.add_api('a1-openapi.yaml')
+app.run(port=port_number)
+
