6d8231a41263b49cb227b8eff2d39b1f3aa778bd
RehanRaza <muhammad.rehan.raza@est.tech>
2019-12-12 11:05:55 +0100

    Add A1 Client in Policy Agent
    Change-Id: I9b10f5c0dbd1d8d0aef748c24a884b8671ef8863
    Issue-ID: NONRTRIC-80
    Signed-off-by: RehanRaza <muhammad.rehan.raza@est.tech>

---
+++ policy-agent/src/main/java/org/oransc/policyagent/clients/AsyncRestClient.java
@@ -0,0 +1,55 @@
+/*-
+ * ========================LICENSE_START=================================
+ * O-RAN-SC
+ * %%
+ * Copyright (C) 2019 Nordix Foundation
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================LICENSE_END===================================
+ */
+package org.oransc.policyagent.clients;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+public class AsyncRestClient {
+    private final WebClient client;
+
+    public AsyncRestClient(String baseUrl) {
+        this.client = WebClient.create(baseUrl);
+    }
+
+    public Mono<String> put(String uri, String body) {
+        return client.put() //
+            .uri(uri) //
+            .contentType(MediaType.APPLICATION_JSON) //
+            .syncBody(body) //
+            .retrieve() //
+            .bodyToMono(String.class);
+    }
+
+    public Mono<String> get(String uri) {
+        return client.get() //
+            .uri(uri) //
+            .retrieve() //
+            .bodyToMono(String.class);
+    }
+
+    public Mono<Void> delete(String uri) {
+        return client.delete() //
+            .uri(uri) //
+            .retrieve() //
+            .bodyToMono(Void.class);
+    }
+}
