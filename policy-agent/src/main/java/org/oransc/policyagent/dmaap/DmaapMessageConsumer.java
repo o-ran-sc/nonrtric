720e346ae0b5cc8deb43c76212e4f8891b6ceaa3
Lathish <lathishbabu.ganesan@est.tech>
2020-01-24 14:50:32 +0000

    Initial Working DMAAP Listener
    Issue-ID: NONRTRIC-107
    Change-Id: I6ebf378d651e19aa236ea86b33855ceac4114587
    Signed-off-by: Lathish <lathishbabu.ganesan@est.tech>

--- policy-agent/src/main/java/org/oransc/policyagent/dmaap/DmaapMessageConsumer.java
+++ policy-agent/src/main/java/org/oransc/policyagent/dmaap/DmaapMessageConsumer.java
@@ -1,22 +1,65 @@
+
+/*-
+ * ========================LICENSE_START=================================
+ * O-RAN-SC
+ * %%
+ * Copyright (C) 2019 Nordix Foundation
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================LICENSE_END===================================
+ */
+
 package org.oransc.policyagent.dmaap;
 
 import java.util.Properties;
 
 /**
- * The Dmaap consumer which has the base methods to be implemented by any class
- * which implements this interface
+ * The Dmaap consumer which has the base methods to be implemented by any class which implements this interface
  *
  */
-public interface DmaapMessageConsumer extends Runnable {
+public interface DmaapMessageConsumer {
 
-	public void init(Properties baseProperties);
+    /**
+     * The init method creates the MRConsumer with the properties passed from the Application Config
+     *
+     * @param properties
+     */
+    public void init(Properties properties);
 
-	public abstract void processMsg(String msg) throws Exception;
+    /**
+     * This method process the message and call the respective Controller
+     *
+     * @param msg
+     * @throws Exception
+     */
+    public abstract void processMsg(String msg) throws Exception;
 
-	public boolean isReady();
+    /**
+     * To check whether the DMAAP Listner is alive
+     *
+     * @return boolean
+     */
+    public boolean isAlive();
 
-	public boolean isRunning();
+    /**
+     * To Stop the DMAAP Listener
+     */
+    public void stopConsumer();
 
-	public void stopConsumer();
+    /**
+     * It's a infinite loop run every configured seconds to fetch the message from DMAAP. This method can be stop by
+     * setting the alive flag to false
+     */
+    public void run();
 
 }
