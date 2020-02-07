f7abf32ee966cdfcee0107331d7d5125b54ec40e
PatrikBuhr <patrik.buhr@est.tech>
2020-02-07 12:59:09 +0100

    Removed an unused interface
    Change-Id: I30166c27546dc584d8ee4675af3d807e1175282f
    Issue-ID: NONRTRIC-107
    Signed-off-by: PatrikBuhr <patrik.buhr@est.tech>

--- policy-agent/src/main/java/org/oransc/policyagent/dmaap/DmaapMessageConsumer.java
+++ policy-agent/src/main/java/org/oransc/policyagent/dmaap/DmaapMessageConsumer.java
@@ -1,4 +1,3 @@
-
 /*-
  * ========================LICENSE_START=================================
  * O-RAN-SC
@@ -21,38 +20,106 @@
 
 package org.oransc.policyagent.dmaap;
 
-/**
- * The Dmaap consumer which has the base methods to be implemented by any class which implements this interface
- *
- */
-public interface DmaapMessageConsumer {
+import com.google.common.collect.Iterables;
 
-    /**
-     * The init method creates the MRConsumer with the properties passed from the Application Config
-     *
-     * @param properties
-     */
-    public void init();
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
-    /**
-     * This method process the message and call the respective Controller
-     *
-     * @param msg
-     * @throws Exception
-     */
-    public abstract void processMsg(String msg) throws Exception;
+import org.onap.dmaap.mr.client.MRBatchingPublisher;
+import org.onap.dmaap.mr.client.MRClientFactory;
+import org.onap.dmaap.mr.client.MRConsumer;
+import org.onap.dmaap.mr.client.response.MRConsumerResponse;
+import org.oransc.policyagent.clients.AsyncRestClient;
+import org.oransc.policyagent.configuration.ApplicationConfig;
+import org.oransc.policyagent.exceptions.ServiceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-    /**
-     * To check whether the DMAAP Listner is alive
-     *
-     * @return boolean
-     */
-    public boolean isAlive();
+@Component
+public class DmaapMessageConsumer implements Runnable {
 
-    /**
-     * It's a infinite loop run every configured seconds to fetch the message from DMAAP. This method can be stop by
-     * setting the alive flag to false
-     */
-    public void run();
+    private static final Logger logger = LoggerFactory.getLogger(DmaapMessageConsumer.class);
 
+    final Duration ERROR_TIMEOUT = Duration.ofSeconds(30);
+    final Duration TIME_BETWEEN_DMAAP_POLLS = Duration.ofSeconds(10);
+    private final ApplicationConfig applicationConfig;
+
+    @Value("${server.port}")
+    private int localServerPort;
+
+    @Autowired
+    public DmaapMessageConsumer(ApplicationConfig applicationConfig) {
+        this.applicationConfig = applicationConfig;
+
+        Thread thread = new Thread(this);
+        thread.start();
+    }
+
+    private boolean isDmaapConfigured() {
+        Properties consumerCfg = applicationConfig.getDmaapConsumerConfig();
+        Properties producerCfg = applicationConfig.getDmaapPublisherConfig();
+        return (consumerCfg != null && consumerCfg.size() > 0 && producerCfg != null && producerCfg.size() > 0);
+    }
+
+    @Override
+    public void run() {
+        while (sleep(TIME_BETWEEN_DMAAP_POLLS) && isDmaapConfigured()) {
+            try {
+                Iterable<String> dmaapMsgs = fetchAllMessages();
+                if (dmaapMsgs != null && Iterables.size(dmaapMsgs) > 0) {
+                    logger.debug("Fetched all the messages from DMAAP and will start to process the messages");
+                    for (String msg : dmaapMsgs) {
+                        processMsg(msg);
+                    }
+                }
+            } catch (Exception e) {
+                logger.error("{}: cannot fetch because of ", this, e.getMessage(), e);
+                sleep(ERROR_TIMEOUT);
+            }
+        }
+    }
+
+    private Iterable<String> fetchAllMessages() throws ServiceException, FileNotFoundException, IOException {
+        Properties dmaapConsumerProperties = this.applicationConfig.getDmaapConsumerConfig();
+        MRConsumer consumer = MRClientFactory.createConsumer(dmaapConsumerProperties);
+        MRConsumerResponse response = consumer.fetchWithReturnConsumerResponse();
+        if (response == null || !"200".equals(response.getResponseCode())) {
+            throw new ServiceException("DMaaP NULL response received");
+        } else {
+            logger.debug("DMaaP consumer received {} : {}", response.getResponseCode(), response.getResponseMessage());
+            return response.getActualMessages();
+        }
+    }
+
+    private void processMsg(String msg) throws Exception {
+        logger.debug("Message Reveived from DMAAP : {}", msg);
+        createDmaapMessageHandler().handleDmaapMsg(msg);
+    }
+
+    private DmaapMessageHandler createDmaapMessageHandler() throws FileNotFoundException, IOException {
+        String agentBaseUrl = "http://localhost:" + this.localServerPort;
+        AsyncRestClient agentClient = new AsyncRestClient(agentBaseUrl);
+        Properties dmaapPublisherProperties = applicationConfig.getDmaapPublisherConfig();
+        MRBatchingPublisher producer = MRClientFactory.createBatchingPublisher(dmaapPublisherProperties);
+
+        return new DmaapMessageHandler(producer, this.applicationConfig, agentClient);
+    }
+
+    private boolean sleep(Duration duration) {
+        CountDownLatch sleep = new CountDownLatch(1);
+        try {
+            sleep.await(duration.toMillis(), TimeUnit.MILLISECONDS);
+            return true;
+        } catch (Exception e) {
+            logger.error("msg", e);
+            return false;
+        }
+    }
 }
