28b508e5df22fd468d18769449710bd0764a778d
elinuxhenrik <henrik.b.andersson@est.tech>
2020-02-26 15:37:17 +0100

    Fix year in licenses
    Change-Id: I402c8209bdd794cd8f796fb4c8342ccd41a22e60
    Issue-ID: NONRTRIC-142
    Signed-off-by: elinuxhenrik <henrik.b.andersson@est.tech>

--- sdnc-a1-controller/northbound/nonrt-ric-api/provider/src/main/java/org/onap/sdnc/northbound/restadapter/NearRicUrlProvider.java
+++ sdnc-a1-controller/northbound/nonrt-ric-api/provider/src/main/java/org/onap/sdnc/northbound/restadapter/NearRicUrlProvider.java
@@ -1,6 +1,6 @@
 /*-
  * ============LICENSE_START=======================================================
- *  Copyright (C) 2019 Nordix Foundation.
+ *  Copyright (C) 2020 Nordix Foundation.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
