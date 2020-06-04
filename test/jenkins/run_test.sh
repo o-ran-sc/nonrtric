379e95deb583e8c95a4c4b47e9ef15a8b95a4d04
ecaiyanlinux <martin.c.yan@est.tech>
2020-06-04 09:34:31 +0200

    prepare for jenkins test
    Signed-off-by: ecaiyanlinux <martin.c.yan@est.tech>
    Issue-ID: NONRTRIC-237
    Change-Id: Iabcc2f514bf1ff73545ddfa6697c23f3a4f5c31c

---
+++ test/jenkins/run_test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#  ============LICENSE_START===============================================
+#  Copyright (C) 2020 Nordix Foundation. All rights reserved.
+#  ========================================================================
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#  ============LICENSE_END=================================================
+#
+
+# This script is to set up test env in jenkins vm
+
+echo "--> run_integration.sh"
+
+# Install docker-compose
+# sudo apt-get update
+# sudo apt-get upgrade
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o ./docker-compose
+export PATH=$PATH:`pwd`
+
+cd ../auto-test/
+./FTC10.sh remote
+echo "--> run_integration.sh END"
