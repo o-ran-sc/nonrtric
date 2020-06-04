09e2ee4bc3c3f22af41be46ecef89adcfe90994b
ecaiyanlinux <martin.c.yan@est.tech>
2020-06-04 09:34:31 +0200

    prepare for jenkins test
    Signed-off-by: ecaiyanlinux <martin.c.yan@est.tech>
    Issue-ID: NONRTRIC-237
    Change-Id: Iabcc2f514bf1ff73545ddfa6697c23f3a4f5c31c

---
+++ test/jenkins/install_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common 
+curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add - 
+sudo add-apt-repository \
+    "deb https://apt.dockerproject.org/repo/ \
+    ubuntu-$(lsb_release -cs) \
+    main" 
+sudo apt-get update
+sudo apt-get -y install docker-engine 
+# add current user to docker group so there is no need to use sudo when running docker
+sudo usermod -aG docker $(whoami)
