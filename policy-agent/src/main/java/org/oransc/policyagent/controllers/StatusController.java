dfbae74ed758978f92159b154c69883bc1d43c5c
Lathish <lathishbabu.ganesan@est.tech>
2020-07-22 10:36:44 +0100

    Updated swagger file with Examples
    Issue-ID: NONRTRIC-260
    Change-Id: I8aea95da2102928a79f4da219d615cfb2c99000d
    Signed-off-by: Lathish <lathishbabu.ganesan@est.tech>

--- policy-agent/src/main/java/org/oransc/policyagent/controllers/StatusController.java
+++ policy-agent/src/main/java/org/oransc/policyagent/controllers/StatusController.java
@@ -24,7 +24,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
