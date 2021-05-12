aea0b4bc5c108e5246be09b1afef6ffc4e812840
RehanRaza <muhammad.rehan.raza@est.tech>
2021-05-12 18:14:46 +0200

    First version of apex policy for O-RU and O-DU usecase
    Change-Id: Ic87e324eba8f711bc59932d58111480b94c49be4
    Issue-ID: NONRTRIC-501
    Signed-off-by: RehanRaza <muhammad.rehan.raza@est.tech>

---
+++ test/usecases/LinkMonitor/models/TaskSelectionLogic.js
@@ -0,0 +1,47 @@
+/*
+ * ============LICENSE_START=======================================================
+ * Copyright (C) 2021 Nordix Foundation.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+executor.logger.info("Task Selection Execution: '"+executor.subject.id+
+    "'. InputFields: '"+executor.inFields+"'");
+
+var linkFailureInput = executor.inFields.get("LinkFailureInput");
+var commonEventHeader = linkFailureInput.get("event").get("commonEventHeader");
+var domain = commonEventHeader.get("domain");
+
+taskFailure = executor.subject.getTaskKey("CreateLinkFailureOutfieldsTask");
+taskCleared = executor.subject.getTaskKey("CreateLinkClearedOutfieldsTask");
+taskDefault = executor.subject.getDefaultTaskKey();
+
+if (domain == "fault") {
+    var faultFields = linkFailureInput.get("event").get("faultFields");
+    var alarmCondition = faultFields.get("alarmCondition");
+    var eventSeverity = faultFields.get("eventSeverity");
+    if (alarmCondition == "30" && eventSeverity != "NORMAL") {
+        taskFailure.copyTo(executor.selectedTask);
+    } else if (alarmCondition == "30" && eventSeverity == "NORMAL") {
+        taskCleared.copyTo(executor.selectedTask);
+    } else {
+        taskDefault.copyTo(executor.selectedTask);
+    }
+} else {
+    taskDefault.copyTo(executor.selectedTask);
+}
+
+true;
