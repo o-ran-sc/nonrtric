207511de83bd3dbb8b9ec55688e38968e1e9d7a6
ecaiyanlinux <martin.c.yan@est.tech>
2021-05-17 16:40:15 +0800

    Add scripts & docker-compose file for oru app and related components
    docker-compose file contains:
    dmaap-mr
    oru-app
    sdnr-simulator
    message-generator
    
    Issue-ID: NONRTRIC-496
    Change-Id: I3692e402d1ac9b846802ad2c6f67cef633742a57
    Signed-off-by: ecaiyanlinux <martin.c.yan@est.tech>

---
+++ test/usecases/linkfailure/docker-compose/start.sh
@@ -0,0 +1,50 @@
+#  Copyright (C) 2021 Nordix Foundation. All rights reserved.
+#  ========================================================================
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#  ============LICENSE_END=================================================
+#
+
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+docker stop $(docker ps -aq)
+docker system prune -f
+
+# build o-ru application image
+cd ${SHELL_FOLDER}/../app/
+docker build -t oru-app .
+
+# build simulator image of sdnr
+cd ${SHELL_FOLDER}/../simulators/
+docker build -f Dockerfile-sdnr-sim -t sdnr-simulator .
+
+# build message generator image
+docker build -f Dockerfile-message-generator -t message-generator .
+
+# start up dmaap message router
+cd ${SHELL_FOLDER}/../../../../docker-compose/
+docker-compose -f docker-compose.yaml -f mr/docker-compose.yml up -d
+
+# wait until mr up & running
+for i in {1..60}; do
+    res=$(curl -o /dev/null -sw %{http_code} http://localhost:3904/topics)
+    echo "$res"
+    expect="200"
+    if [ "$res" == "$expect" ]; then
+        echo -e "dmaap-mr is alive!\n"
+        break;
+    else
+        sleep $i
+    fi
+done
+
+cd ${SHELL_FOLDER}
+docker-compose up -d
