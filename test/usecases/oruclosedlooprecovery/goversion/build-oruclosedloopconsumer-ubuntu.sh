6e323a6d8f04cec11b5f3f1a7fcb7c5f6b10247d
elinuxhenrik <henrik.b.andersson@est.tech>
2021-11-05 14:17:05 +0100

    Fix of Sonar coverage for O-RU Closed Loop Consumer
    Issue-ID: NONRTRIC-620
    Signed-off-by: elinuxhenrik <henrik.b.andersson@est.tech>
    Change-Id: Id6765d0b741fbce7f29cdcf7c5899dce483e05ca

--- test/usecases/oruclosedlooprecovery/goversion/build-oruclosedloopconsumer-ubuntu.sh
+++ test/usecases/oruclosedlooprecovery/goversion/build-oruclosedloopconsumer-ubuntu.sh
@@ -34,7 +34,7 @@ go get github.com/stretchr/testify/mock@v1.7.0
 
 go-acc ./... --ignore mocks
 
-sed -i -e 's/oransc\.org\/usecase\/oruclosedloop/oruclosedloop/' coverage.txt
+sed -i -e 's/oransc\.org\/usecase\/oruclosedloop/test\/usecases\/oruclosedlooprecovery\/goversion/' coverage.txt
 
 cp coverage.txt $curdir
 echo "--> build-oruclosedloopconsumer-ubuntu.sh ends"
