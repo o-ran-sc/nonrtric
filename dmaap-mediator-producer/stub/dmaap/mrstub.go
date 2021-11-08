52ed923b1d35e15f7dd6d367a5bd0da15b9148c3
elinuxhenrik <henrik.b.andersson@est.tech>
2021-11-08 15:58:20 +0100

    Introduce choice between secure and non secure com
    The DMaaP Mediator producer now uses secure communication towards any
    address configured to use the https scheme. If its own callback is
    configured to the https scheme, it will only listen to an https port,
    otherwise it will only use an http port.
    
    Also moved the certificate and key files to a separate folder.
    
    Issue-ID: NONRTRIC-633
    Signed-off-by: elinuxhenrik <henrik.b.andersson@est.tech>
    Change-Id: Id3bb18e83d40d26740b71be7901911c9a41502e3

--- dmaap-mediator-producer/stub/dmaap/mrstub.go
+++ dmaap-mediator-producer/stub/dmaap/mrstub.go
@@ -57,7 +57,7 @@ func main() {
 	http.HandleFunc("/events/unauthenticated.SEC_FAULT_OUTPUT/dmaapmediatorproducer/STD_Fault_Messages", handleData)
 
 	fmt.Print("Starting mr on port: ", *port)
-	http.ListenAndServeTLS(fmt.Sprintf(":%v", *port), "../../configs/producer.crt", "../../configs/producer.key", nil)
+	http.ListenAndServeTLS(fmt.Sprintf(":%v", *port), "../../security/producer.crt", "../../security/producer.key", nil)
 
 }
 
