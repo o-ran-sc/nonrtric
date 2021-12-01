0d13d64cb3fa42398da62a08320f6fe19a66b44f
elinuxhenrik <henrik.b.andersson@est.tech>
2021-12-01 07:36:37 +0100

    Update docs after name change of ICS
    Issue-ID: NONRTRIC-656
    Signed-off-by: elinuxhenrik <henrik.b.andersson@est.tech>
    Change-Id: I58820623d139b2ee0c90425c2b794bb2b9241200

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ linkcheck_ignore = [
     'http://127.0.0.1.*',
     'https://gerrit.o-ran-sc.org.*',
     './rac-api.html', #Generated file that doesn't exist at link check.
-    './ecs-api.html' #Generated file that doesn't exist at link check.
+    './ics-api.html' #Generated file that doesn't exist at link check.
 ]
 
 extensions = ['sphinxcontrib.redoc', 'sphinx.ext.intersphinx',]
@@ -23,7 +23,7 @@ redoc = [
             },
             {
                 'name': 'ECS API',
-                'page': 'ecs-api',
+                'page': 'ics-api',
                 'spec': '../information-coordinator-service/api/ics-api.json',
                 'embed': True,
             }
