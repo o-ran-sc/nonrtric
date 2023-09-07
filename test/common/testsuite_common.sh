6d39590f99e9ccc23ebf941314caf096841ce211
rohithrajneesh <rohith.rajneesh@est.tech>
2023-09-07 10:11:56 +0100

    Adding a suite of tests for the nonrtric verify jobs
    Issue-ID: NONRTRIC-918
    Change-Id: I84c01ff2dfed81fa4652e970965b96e60b707148
    Signed-off-by: rohithrajneesh <rohith.rajneesh@est.tech>

--- test/common/testsuite_common.sh
+++ test/common/testsuite_common.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #  ============LICENSE_START===============================================
-#  Copyright (C) 2020 Nordix Foundation. All rights reserved.
+#  Copyright (C) 2023 Nordix Foundation. All rights reserved.
 #  ========================================================================
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
