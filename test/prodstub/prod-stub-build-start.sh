d30570e0aef589a471d12762b801f9354700c38c
ambrishest <ambrish.singh@est.tech>
2023-12-07 08:33:45 +0000

    Hello World service for NONRTRIC - Unit Tests Added
    Issue-ID: NONRTRIC-944
    Change-Id: I59122a202746587a34d461ef84fd331bf7c5a73e
    Signed-off-by: ambrishest <ambrish.singh@est.tech>

--- test/prodstub/prod-stub-build-start.sh
+++ test/prodstub/prod-stub-build-start.sh
@@ -22,6 +22,6 @@
 NAME="producer-stub-test"
 IMAGE_NAME="producer-stub-test-image"
 
-docker build --build-arg NEXUS_PROXY_REPO=nexus3.onap.org:10001/ -t $IMAGE_NAME .
+docker build -t $IMAGE_NAME .
 
 docker run --rm -it -p 8992:8092 -p 8993:8093 --name $NAME $IMAGE_NAME
