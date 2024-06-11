7168be36cf75334ac24d3543f7359161d01779f3
raviteja.karumuri <raviteja.karumuri@est.tech>
2024-06-11 13:11:20 +0100

    Updating FTC 110 series tests to support a1pms V3 image
    Issue-ID: NONRTRIC-1013
    Change-Id: I38f728245c7d6141a2468048f4ab991fa5015458
    Signed-off-by: Raviteja Karumuri <raviteja.karumuri@est.tech>

--- test/auto-test/FTC110.sh
+++ test/auto-test/FTC110.sh
@@ -44,211 +44,245 @@ SUPPORTED_RUNMODES="DOCKER KUBE"
 setup_testenvironment
 
 #### TEST BEGIN ####
+VERSIONS_TO_RUN=2
 
-sim_generate_policy_uuid
-
-use_cr_http
-use_simulator_http
-use_mr_http
-use_a1pms_rest_http
-
-clean_environment
-
-start_kube_proxy
-
-start_ric_simulators ricsim_g1 1  OSC_2.1.0
-start_ric_simulators ricsim_g2 1  STD_1.1.3
-start_ric_simulators ricsim_g3 1  STD_2.0.0
-
-if [[ "$A1PMS_FEATURE_LEVEL" == *"NO-DMAAP"* ]]; then
-    :
-else
-    start_mr
+if [ -n "$A1PMS_V3_FLAG" ] && [ "${A1PMS_V3_FLAG,,}" == "true" ]; then
+  VERSIONS_TO_RUN=3
 fi
 
-start_cr 1
+for version in $(seq 2 $VERSIONS_TO_RUN); do
+  if [ "$version" == "3" ]; then
+    if [ -n "$A1PMS_V3" ] && [ -n "$A1PMS_API_PREFIX_V3" ] && [ -n "$A1PMS_ALIVE_URL_V3" ]; then
+      export A1PMS_VERSION="$A1PMS_V3"
+      export A1PMS_API_PREFIX="$A1PMS_API_PREFIX_V3"
+      export A1PMS_ALIVE_URL="$A1PMS_API_PREFIX$A1PMS_ALIVE_URL_V3"
+    else
+      echo "One/All parameters not set in env file (<A1PMS_V3>, <A1PMS_API_PREFIX_V3>, <A1PMS_ALIVE_URL_V3>)"
+      break
+    fi
+  fi
+  sim_generate_policy_uuid
 
-start_control_panel $SIM_GROUP/$CONTROL_PANEL_COMPOSE_DIR/$CONTROL_PANEL_CONFIG_FILE
+  use_cr_http
+  use_simulator_http
+  use_mr_http
+  use_a1pms_rest_http
 
-if [ ! -z "$NRT_GATEWAY_APP_NAME" ]; then
-    start_gateway $SIM_GROUP/$NRT_GATEWAY_COMPOSE_DIR/$NRT_GATEWAY_CONFIG_FILE
-fi
+  clean_environment
 
-start_a1pms NORPOXY $SIM_GROUP/$A1PMS_COMPOSE_DIR/$A1PMS_CONFIG_FILE
+  start_kube_proxy
 
+  start_ric_simulators ricsim_g1 1  OSC_2.1.0
+  start_ric_simulators ricsim_g2 1  STD_1.1.3
+  start_ric_simulators ricsim_g3 1  STD_2.0.0
 
-prepare_a1pms_config      NOSDNC  ".a1pms_config.json"
+  if [[ "$A1PMS_FEATURE_LEVEL" == *"NO-DMAAP"* ]]; then
+      :
+  else
+      start_mr
+  fi
 
-if [ $RUNMODE == "KUBE" ]; then
-    a1pms_load_config                       ".a1pms_config.json"
-else
-    a1pms_api_put_configuration 200 ".a1pms_config.json"
-fi
+  start_cr 1
 
-set_a1pms_debug
+  start_control_panel $SIM_GROUP/$CONTROL_PANEL_COMPOSE_DIR/$CONTROL_PANEL_CONFIG_FILE
 
-if [[ "$A1PMS_FEATURE_LEVEL" == *"NO-DMAAP"* ]]; then
-    :
-else
-    mr_equal requests_submitted 0
-fi
+  if [ ! -z "$NRT_GATEWAY_APP_NAME" ]; then
+      start_gateway $SIM_GROUP/$NRT_GATEWAY_COMPOSE_DIR/$NRT_GATEWAY_CONFIG_FILE
+  fi
 
-sleep_wait 120 "Let A1PMS configuration take effect"
+  start_a1pms NORPOXY $SIM_GROUP/$A1PMS_COMPOSE_DIR/$A1PMS_CONFIG_FILE
 
-#Check a1pms alive
-a1pms_api_get_status 200
 
-#Print simulator interface version
-sim_print ricsim_g1_1 interface
-sim_print ricsim_g2_1 interface
-sim_print ricsim_g3_1 interface
+  prepare_a1pms_config      NOSDNC  ".a1pms_config.json"
 
-a1pms_api_put_service 201 "service1" 15 "$CR_SERVICE_APP_PATH_0/service1"
+  if [ $RUNMODE == "KUBE" ]; then
+      a1pms_load_config                       ".a1pms_config.json"
+  else
+      a1pms_api_put_configuration 200 ".a1pms_config.json"
+  fi
 
-a1pms_api_get_services 200 "service1" "service1" 15 "$CR_SERVICE_APP_PATH_0/service1"
+  set_a1pms_debug
 
-a1pms_api_put_service 201 "service2" 120 "$CR_SERVICE_APP_PATH_0/service2"
+  if [[ "$A1PMS_FEATURE_LEVEL" == *"NO-DMAAP"* ]]; then
+      :
+  else
+      mr_equal requests_submitted 0
+  fi
 
-a1pms_api_get_services 200 "service2" "service2" 120 "$CR_SERVICE_APP_PATH_0/service2"
+  sleep_wait 120 "Let A1PMS configuration take effect"
 
-a1pms_api_put_service 200 "service1" 50 "$CR_SERVICE_APP_PATH_0/service1"
-a1pms_api_put_service 200 "service2" 180 "$CR_SERVICE_APP_PATH_0/service2"
+  #Check a1pms alive
+  a1pms_api_get_status 200
 
-a1pms_api_get_services 200 "service1" "service1" 50 "$CR_SERVICE_APP_PATH_0/service1"
-a1pms_api_get_services 200 "service2" "service2" 180 "$CR_SERVICE_APP_PATH_0/service2"
+  #Print simulator interface version
+  sim_print ricsim_g1_1 interface
+  sim_print ricsim_g2_1 interface
+  sim_print ricsim_g3_1 interface
 
-a1pms_api_get_service_ids 200 "service1" "service2"
+  a1pms_api_put_service 201 "service1" 15 "$CR_SERVICE_APP_PATH_0/service1"
 
-sleep_wait 30 "Waiting for keep alive timeout"
+  a1pms_api_get_services 200 "service1" "service1" 15 "$CR_SERVICE_APP_PATH_0/service1"
 
-a1pms_api_get_services 200 "service1" "service1" 50 "$CR_SERVICE_APP_PATH_0/service1"
-a1pms_api_get_services 200 "service2" "service2" 180 "$CR_SERVICE_APP_PATH_0/service2"
+  a1pms_api_put_service 201 "service2" 120 "$CR_SERVICE_APP_PATH_0/service2"
 
-sleep_wait 100 "Waiting for keep alive timeout"
+  a1pms_api_get_services 200 "service2" "service2" 120 "$CR_SERVICE_APP_PATH_0/service2"
 
-a1pms_api_get_services 404 "service1"
-a1pms_api_get_services 200 "service2" "service2" 180 "$CR_SERVICE_APP_PATH_0/service2"
+  a1pms_api_put_service 200 "service1" 50 "$CR_SERVICE_APP_PATH_0/service1"
+  a1pms_api_put_service 200 "service2" 180 "$CR_SERVICE_APP_PATH_0/service2"
 
-a1pms_api_delete_services 204 "service2"
+  a1pms_api_get_services 200 "service1" "service1" 50 "$CR_SERVICE_APP_PATH_0/service1"
+  a1pms_api_get_services 200 "service2" "service2" 180 "$CR_SERVICE_APP_PATH_0/service2"
 
-a1pms_api_get_services 404 "service1"
-a1pms_api_get_services 404 "service2"
+  a1pms_api_get_service_ids 200 "service1" "service2"
 
-a1pms_api_put_service 201 "service3" 60 "$CR_SERVICE_APP_PATH_0/service3"
+  sleep_wait 30 "Waiting for keep alive timeout"
 
-a1pms_api_get_services 200 "service3" "service3" 60 "$CR_SERVICE_APP_PATH_0/service3"
+  a1pms_api_get_services 200 "service1" "service1" 50 "$CR_SERVICE_APP_PATH_0/service1"
+  a1pms_api_get_services 200 "service2" "service2" 180 "$CR_SERVICE_APP_PATH_0/service2"
 
-sleep_wait 30 "Waiting for keep alive timeout"
+  sleep_wait 100 "Waiting for keep alive timeout"
 
-a1pms_api_put_service 200 "service3" 60 "$CR_SERVICE_APP_PATH_0/service3"
+  a1pms_api_get_services 404 "service1"
+  a1pms_api_get_services 200 "service2" "service2" 180 "$CR_SERVICE_APP_PATH_0/service2"
 
-sleep_wait 100 "Waiting for keep alive timeout"
+  a1pms_api_delete_services 204 "service2"
 
-a1pms_api_get_services 404 "service3"
+  a1pms_api_get_services 404 "service1"
+  a1pms_api_get_services 404 "service2"
 
-a1pms_api_put_service 201 "service4" 120 "$CR_SERVICE_APP_PATH_0/service4"
+  a1pms_api_put_service 201 "service3" 60 "$CR_SERVICE_APP_PATH_0/service3"
 
-sleep_wait 60 "Waiting for keep alive timeout"
+  a1pms_api_get_services 200 "service3" "service3" 60 "$CR_SERVICE_APP_PATH_0/service3"
 
-a1pms_api_get_services 200 "service4" "service4" 120 "$CR_SERVICE_APP_PATH_0/service4"
+  sleep_wait 30 "Waiting for keep alive timeout"
 
-a1pms_api_put_services_keepalive 200 "service4"
+  a1pms_api_put_service 200 "service3" 60 "$CR_SERVICE_APP_PATH_0/service3"
 
-sleep_wait 90 "Waiting for keep alive timeout"
+  sleep_wait 100 "Waiting for keep alive timeout"
 
-a1pms_api_get_services 200 "service4" "service4" 120 "$CR_SERVICE_APP_PATH_0/service4"
+  a1pms_api_get_services 404 "service3"
 
-a1pms_api_delete_services 204 "service4"
+  a1pms_api_put_service 201 "service4" 120 "$CR_SERVICE_APP_PATH_0/service4"
 
-a1pms_api_get_services 404 "service4"
+  sleep_wait 60 "Waiting for keep alive timeout"
 
-a1pms_api_get_services 404 "service1"
-a1pms_api_get_services 404 "service2"
-a1pms_api_get_services 404 "service3"
+  a1pms_api_get_services 200 "service4" "service4" 120 "$CR_SERVICE_APP_PATH_0/service4"
 
-a1pms_api_get_service_ids 200
+  a1pms_api_put_services_keepalive 200 "service4"
 
-a1pms_api_delete_services 404 "service1"
-a1pms_api_delete_services 404 "service2"
-a1pms_api_delete_services 404 "service3"
-a1pms_api_delete_services 404 "service4"
+  sleep_wait 90 "Waiting for keep alive timeout"
 
-a1pms_api_put_services_keepalive 404 "service1"
-a1pms_api_put_services_keepalive 404 "service2"
-a1pms_api_put_services_keepalive 404 "service3"
-a1pms_api_put_services_keepalive 404 "service4"
+  a1pms_api_get_services 200 "service4" "service4" 120 "$CR_SERVICE_APP_PATH_0/service4"
 
-# Policy delete after timeout
-a1pms_api_put_service 201 "service10" 600 "$CR_SERVICE_APP_PATH_0/service10"
+  a1pms_api_delete_services 204 "service4"
 
-sim_put_policy_type 201 ricsim_g1_1 1 testdata/OSC/sim_1.json
+  a1pms_api_get_services 404 "service4"
 
-sim_put_policy_type 201 ricsim_g3_1 STD_QOS2_0.1.0 testdata/STD2/sim_qos2.json
+  a1pms_api_get_services 404 "service1"
+  a1pms_api_get_services 404 "service2"
+  a1pms_api_get_services 404 "service3"
 
-a1pms_equal json:rics 3 300
+  a1pms_api_get_service_ids 200
 
-#a1pms_equal json:policy_schemas 2 120
+  a1pms_api_delete_services 404 "service1"
+  a1pms_api_delete_services 404 "service2"
+  a1pms_api_delete_services 404 "service3"
+  a1pms_api_delete_services 404 "service4"
 
-a1pms_equal json:policy-types 3 120
+  a1pms_api_put_services_keepalive 404 "service1"
+  a1pms_api_put_services_keepalive 404 "service2"
+  a1pms_api_put_services_keepalive 404 "service3"
+  a1pms_api_put_services_keepalive 404 "service4"
 
-a1pms_equal json:policies 0
+  # Policy delete after timeout
+  a1pms_api_put_service 201 "service10" 600 "$CR_SERVICE_APP_PATH_0/service10"
 
-notificationurl=$CR_SERVICE_APP_PATH_0"/test"
+  sim_put_policy_type 201 ricsim_g1_1 1 testdata/OSC/sim_1.json
 
-a1pms_api_put_policy 201 "service10" ricsim_g1_1 1 5000 NOTRANSIENT $notificationurl testdata/OSC/pi1_template.json
-a1pms_api_put_policy 201 "service10" ricsim_g2_1 NOTYPE 5100 NOTRANSIENT $notificationurl testdata/STD/pi1_template.json
+  sim_put_policy_type 201 ricsim_g3_1 STD_QOS2_0.1.0 testdata/STD2/sim_qos2.json
 
-a1pms_api_put_policy 201 "service10" ricsim_g3_1 STD_QOS2_0.1.0 5200 NOTRANSIENT $notificationurl testdata/STD2/pi_qos2_template.json
-a1pms_equal json:policies 3
+  a1pms_equal json:rics 3 300
 
-sim_equal ricsim_g1_1 num_instances 1
-sim_equal ricsim_g2_1 num_instances 1
+  #a1pms_equal json:policy_schemas 2 120
 
-a1pms_api_put_policy 201 "service10" ricsim_g1_1 1 5001 true $notificationurl testdata/OSC/pi1_template.json
-a1pms_api_put_policy 201 "service10" ricsim_g2_1 NOTYPE 5101 true $notificationurl testdata/STD/pi1_template.json
+  a1pms_equal json:policies 0
 
-a1pms_api_put_policy 201 "service10" ricsim_g3_1 STD_QOS2_0.1.0 5201 true $notificationurl testdata/STD2/pi_qos2_template.json
-a1pms_equal json:policies 6
+  notificationurl=$CR_SERVICE_APP_PATH_0"/test"
 
-sim_equal ricsim_g1_1 num_instances 2
-sim_equal ricsim_g2_1 num_instances 2
-sim_equal ricsim_g3_1 num_instances 2
+  if [ "$A1PMS_VERSION" == "V3" ]; then
+    a1pms_equal json:policytypes 3 120
+    a1pms_api_post_policy_v3 201 "service10" ricsim_g1_1 1 5000 NOTRANSIENT $notificationurl testdata/OSC/pi1_template.json
+    a1pms_api_post_policy_v3 201 "service10" ricsim_g2_1 NOTYPE 5100 NOTRANSIENT $notificationurl testdata/STD/pi1_template.json
 
-sim_post_delete_instances 200 ricsim_g1_1
-sim_post_delete_instances 200 ricsim_g2_1
+    a1pms_api_post_policy_v3 201 "service10" ricsim_g3_1 STD_QOS2_0.1.0 5200 NOTRANSIENT $notificationurl testdata/STD2/pi_qos2_template.json
 
-sim_post_delete_instances 200 ricsim_g3_1
+  else
+    a1pms_equal json:policy-types 3 120
+    a1pms_api_put_policy 201 "service10" ricsim_g1_1 1 5000 NOTRANSIENT $notificationurl testdata/OSC/pi1_template.json
+    a1pms_api_put_policy 201 "service10" ricsim_g2_1 NOTYPE 5100 NOTRANSIENT $notificationurl testdata/STD/pi1_template.json
 
-#Wait for recreate of non transient policy
-a1pms_equal json:policies 3 180
+    a1pms_api_put_policy 201 "service10" ricsim_g3_1 STD_QOS2_0.1.0 5200 NOTRANSIENT $notificationurl testdata/STD2/pi_qos2_template.json
 
-sim_equal ricsim_g1_1 num_instances 1
-sim_equal ricsim_g2_1 num_instances 1
-sim_equal ricsim_g3_1 num_instances 1
+  fi
 
-a1pms_api_put_service 200 "service10" 10 "$CR_SERVICE_APP_PATH_0/service10"
+  a1pms_equal json:policies 3
 
-#Wait for service expiry
-a1pms_equal json:policies 0 120
+  sim_equal ricsim_g1_1 num_instances 1
+  sim_equal ricsim_g2_1 num_instances 1
 
-sim_equal ricsim_g1_1 num_instances 0
-sim_equal ricsim_g2_1 num_instances 0
-sim_equal ricsim_g3_1 num_instances 0
+  if [ "$A1PMS_VERSION" == "V3" ]; then
+    a1pms_api_post_policy_v3 201 "service10" ricsim_g1_1 1 5001 true $notificationurl testdata/OSC/pi1_template.json
+    a1pms_api_post_policy_v3 201 "service10" ricsim_g2_1 NOTYPE 5101 true $notificationurl testdata/STD/pi1_template.json
 
-a1pms_api_get_service_ids 200
+    a1pms_api_post_policy_v3 201 "service10" ricsim_g3_1 STD_QOS2_0.1.0 5201 true $notificationurl testdata/STD2/pi_qos2_template.json
+  else
+    a1pms_api_put_policy 201 "service10" ricsim_g1_1 1 5001 true $notificationurl testdata/OSC/pi1_template.json
+    a1pms_api_put_policy 201 "service10" ricsim_g2_1 NOTYPE 5101 true $notificationurl testdata/STD/pi1_template.json
 
-if [[ "$A1PMS_FEATURE_LEVEL" == *"NO-DMAAP"* ]]; then
-    :
-else
-    mr_equal requests_submitted 0
-fi
+    a1pms_api_put_policy 201 "service10" ricsim_g3_1 STD_QOS2_0.1.0 5201 true $notificationurl testdata/STD2/pi_qos2_template.json
+  fi
+  a1pms_equal json:policies 6
 
-check_a1pms_logs
+  sim_equal ricsim_g1_1 num_instances 2
+  sim_equal ricsim_g2_1 num_instances 2
+  sim_equal ricsim_g3_1 num_instances 2
 
-#### TEST COMPLETE ####
+  sim_post_delete_instances 200 ricsim_g1_1
+  sim_post_delete_instances 200 ricsim_g2_1
 
-store_logs          END
+  sim_post_delete_instances 200 ricsim_g3_1
 
-print_result
+  #Wait for recreate of non transient policy
+  a1pms_equal json:policies 3 180
 
-auto_clean_environment
+  sim_equal ricsim_g1_1 num_instances 1
+  sim_equal ricsim_g2_1 num_instances 1
+  sim_equal ricsim_g3_1 num_instances 1
+
+  a1pms_api_put_service 200 "service10" 10 "$CR_SERVICE_APP_PATH_0/service10"
+
+  #Wait for service expiry
+  a1pms_equal json:policies 0 120
+
+  sim_equal ricsim_g1_1 num_instances 0
+  sim_equal ricsim_g2_1 num_instances 0
+  sim_equal ricsim_g3_1 num_instances 0
+
+  a1pms_api_get_service_ids 200
+
+  if [[ "$A1PMS_FEATURE_LEVEL" == *"NO-DMAAP"* ]]; then
+      :
+  else
+      mr_equal requests_submitted 0
+  fi
+
+  check_a1pms_logs
+
+  #### TEST COMPLETE ####
+
+  store_logs          END
+
+  print_result
+
+  auto_clean_environment
+done
