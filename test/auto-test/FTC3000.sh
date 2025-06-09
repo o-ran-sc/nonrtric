a5fd5dad29a760762b6a54d3f5af503f9e6ee33e
saul.gill <saul.gill@est.tech>
2025-06-09 13:18:46 +0100

    Creating PARIS release in ONAP env file for FTC
    Issue-ID: NONRTRIC-1079
    Change-Id: I21a86a59fc139e0c655749aecaf64bad1cd0d4ec
    Signed-off-by: saul.gill <saul.gill@est.tech>
    (cherry picked from commit 4137319326eaeaabcd0c036da2bd45d7003725a4)

--- test/auto-test/FTC3000.sh
+++ test/auto-test/FTC3000.sh
@@ -2,7 +2,7 @@
 
 #  ============LICENSE_START===============================================
 #  Copyright (C) 2020-2023 Nordix Foundation. All rights reserved.
-#  Copyright (C) 2023 OpenInfra Foundation Europe. All rights reserved.
+#  Copyright (C) 2023-2025 OpenInfra Foundation Europe. All rights reserved.
 #  ========================================================================
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
